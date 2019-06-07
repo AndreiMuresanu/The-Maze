22d6fb9373dd3a34a1ec179cb726430a903e4225
AtomicAndrei <44536960+AtomicAndrei@users.noreply.github.com>
2019-06-07 12:15:46 -0400

    Add files via upload

---
+++ Assets/Animations/SwordGhost/SwordGhostAnimationManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordGhostAnimationManager : MonoBehaviour {
+
+	Animator anim;
+	public Transform Switch;
+	public Transform Switch1;
+	public Transform Body;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	void Update () {
+		if(Switch1.position == Body.TransformPoint (1,0,0) && Switch.position == Body.TransformPoint (0,0,0)){
+			anim.SetInteger("CurrentAnimation", 1);
+		}
+		if(Switch1.position == Body.TransformPoint (1,0,0) && Switch.position == Body.TransformPoint (1,0,0)){
+			anim.SetInteger("CurrentAnimation", 1);
+		}
+		if(Switch1.position == Body.TransformPoint (1,0,0) && Switch.position == Body.TransformPoint (2,0,0)){
+			anim.SetInteger("CurrentAnimation", 1);
+		}
+		if(Switch1.position == Body.TransformPoint (0,0,0)){
+			anim.SetInteger("CurrentAnimation", 0);
+		}
+		if(Switch.position == Body.TransformPoint (4,0,0)){
+			anim.SetInteger("CurrentAnimation", 4);
+		}
+	}
+}
---
+++ Assets/Animations/Zombie/ZombieAnimationManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAnimationManager : MonoBehaviour {
+
+	Animator anim;
+	public Transform Switch;
+	public Transform Switch1;
+	public Transform RealBody;
+	bool a = false;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	void Update () {
+		if(Switch1.position == RealBody.TransformPoint (1,0,0)){
+			anim.SetInteger("ZombieCurrentAnimation", 2);
+			//StartCoroutine("Wait");
+		}
+		if(Switch1.position == RealBody.TransformPoint (0,0,0)){
+			anim.SetInteger("ZombieCurrentAnimation", 0);
+		}
+		if(RealBody.GetComponent<Rigidbody>().velocity != Vector3.zero && (Switch1.position != RealBody.TransformPoint (1,0,0)
+		 && Switch.position != RealBody.TransformPoint (0,0,0)) || (Switch1.position != RealBody.TransformPoint (1,0,0)
+		  && Switch.position != RealBody.TransformPoint (1,0,0))){
+			anim.SetInteger("ZombieCurrentAnimation", 1);
+		}
+		if(Switch.position == RealBody.TransformPoint (4,0,0)){
+			anim.SetInteger("ZombieCurrentAnimation", 4);
+		}
+	}
+
+	IEnumerator Wait ()
+	{
+		yield return new WaitForSeconds (0.3f);
+		if (Switch1.position == RealBody.TransformPoint (1, 0, 0)) {
+			a = true;
+		} 
+		else {
+			a = false;
+		}
+		yield return null;
+	}
+}
